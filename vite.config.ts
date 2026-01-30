235ee68393f0274f0895df40c0a5c7430ca3ad4f
Atharv Ingale <atharv.i@humro.com>
2026-01-30 11:25:30 +0530

    changes in  base name

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig(({ mode }) => ({
     port: 8080,
   },
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
-   base:"/venkteshwara-tours---travels/",
+   base:"/venkteshwara-tours-travels/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
